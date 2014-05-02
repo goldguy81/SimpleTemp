814ff4bf3a5d4f701aa01ae48d6f4f7c36397203
goldguy81 <goldguy813@gmail.com>
2014-05-02 10:11:32 -0400

    What I had Before

---
+++ SimpleTemp.py
@@ -0,0 +1,15 @@
+C = (F-32)* 5 / 9
+F = C * 9/5 + 32
+def F2C(F)
+"""Returns the Celsius from Fahrenheit"""
+if F:
+    return C
+else:
+    return "null"
+
+def C2F(C)
+"""Returns the Fahrenheit from Celsius"""
+if C:
+    return F
+else:
+    return "null"
